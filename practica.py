daa50e67e3fbaac6b1cb38b532827b3705b3fe75
Reyner D Paz <ing.reyner.de.paz@gmail.com>
2022-09-14 20:31:35 -0500

    de local/remoto

---
+++ practica.py
@@ -0,0 +1,20 @@
+
+#x = int(input('Cantidad de Iteraciones '))
+# suma=0
+# for n in range(1001):
+#   suma+=n
+#   print(suma) 
+
+# x = int(input('Cantidad de Iteraciones '))
+# suma=0
+# for n in range(x+1):
+#   suma+=n
+
+# print(suma) 
+
+
+
+try:
+  x = int(input('Cantidad de Iteraciones '))
+except:
+  print("Debe Ingresar valor numerico") 
